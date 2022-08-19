5484420b2cec3f265d15481d9d11a0744d90ffd6
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-08-19 18:53:05 +0530

    make h1

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
   render() {
     return (
-      <div>Navbar</div>
+        <div>
+            <h1>Navbar</h1>
+      </div>
     )
   }
 }
