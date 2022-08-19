ede783fa0c94a48140965beef7a24e5f6d66530f
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-08-19 19:16:18 +0530

    Co-authored-by: ak-grg <ak-grg@users.noreply.github.com>

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
 class Navbar extends Component {
-  render() {
-    return (
-        <div className="navbar bg-primary">
-            <h1>Navbar</h1>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="navbar bg-primary">
+                <h1>
+                    <i className="fab fa-github" />
+                    Navbar</h1>
+            </div>
+        )
+    }
 }
 
 export default Navbar
